3cb437644861d45db2e02150cf7d9d7ae4484597
ritukanchi <ritukanchi04@gmail.com>
2024-10-28 18:34:12 +0530

    updated auth, created page

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -13,13 +13,73 @@
 />
 
 <div class ="main">
-    <p>
-        welcome!
-    </p>
+    <div class = "left-article">
+        <div class="heading">
+            <h1>CAUTION!</h1>
+        </div>
+        <div class="image-container">
+            <img src="laptop.png" alt="Description of image" class="my-image" />
+        </div>
+    </div>
+    <div class="right-article">
+        <div class="head-1">
+            <h1>Catch up on the Latest Article!</h1>
+        </div>
+        <div class ="head-2">
+            <h2>don't panic about lead in tampons!</h2>
+        </div>
+        <div class="head-3">
+            <h2>Meftal spas is totally safe</h2>
+        </div>
+    </div>
 </div>
 <style>
     .main{
-        display: flex;
+        display: inline-flex;
+        justify-content: space-around;
+        align-items: center;
+        padding: 60px;
+        gap:900px;
+    }
+    .left-article{
+        display:block;
+        justify-content: left;
+        align-items: top;
+    }
+    .heading{
+        display:baseline;
+        justify-content: left;
+        align-items: top;
+        width: 100%;
+        padding: 10px; 
+        margin: 0px; 
+    }
+    .image-container{
+        padding: 10px; 
+        margin-top: -90px;
+    }
+    .my-image {
+        width: 250%;
+        border-radius: 8px;
+    }
+    .right-article{
+        display: inline;
+        justify-content: right;
+        align-items: right;
+        padding: 10px;
+        width:-400px; 
+    }
+    .head-1{
+        background-color: palegoldenrod;
+        padding: 20px; 
+    }
+    .head-2{
+        padding: 20px;
+        justify-content: center;
+        align-items: center;
+    }
+    .head-3{
+        padding: 20px; 
         justify-content: center;
         align-items: center;
     }
